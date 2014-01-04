73bf582a5c7b9a88383ad792416212b6d7714f97
Satoshi Egi <egi@egison.org>
2014-01-04 15:26:18 +0900

    update

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/json'
 require 'rake'
 require 'github_api'
+require 'open-uri'
 
 set :port, 3010
 set :json_content_type, :js
@@ -26,6 +27,12 @@ get '/github' do
   github = Github.new
   response = github.repos.get("egisatoshi", "egison3/contents/" + params[:path])
   content = Base64.decode64(response.content)
+  ret = { content: content }
+  json ret
+end
+
+get '/source' do
+  content = open("http://www.egison.org/source/" + params[:path]).read
   ret = { content: content }
   json ret
 end
