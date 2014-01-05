2c264c31f8e7c6e0beb93ad82ad8d33a41532b77
Satoshi Egi <egi@egison.org>
2014-01-05 09:25:17 +0900

    update

--- test.rb
+++ test.rb
@@ -1,6 +1,6 @@
 require 'rest_client'
 
-p RestClient.get 'http://www.egison.org/api/version'
+p RestClient.get 'http://api.egison.org/version'
 
-p RestClient.post 'http://www.egison.org/api/eval', {program: "(test (match-all {1 2 3} (list integer) [<cons $x $xs> [x xs]]))"}
+p RestClient.post 'http://api.egison.org/eval', {program: "(test (match-all {1 2 3} (list integer) [<cons $x $xs> [x xs]]))"}
 
